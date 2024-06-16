394eba413fe43de0e0dc9ab1c1ffba9408271b1f
ApVarotariya <apvarotariya17@gmail.com>
2024-06-16 18:24:36 +0530

    change in admin auth

--- src/app/Auth/Admin/Profile/page.jsx
+++ src/app/Auth/Admin/Profile/page.jsx
@@ -9,19 +9,15 @@ import { setLogout } from "@/lib/slices/adminSlice";
 export default function AdminProfile() {
   const dispatch = useDispatch();
   const router = useRouter();
-  const authData = useSelector(selectAdminData);
-  const auth = authData?.admin?.data?.user;
-  console.log("profile", authData);
+  const authState = useSelector(selectAdminData);
+  const auth = authState?.admin;
+  const authData = auth?.data?.user;
 
-  // useEffect(() => {
-  //   if (!auth) {
-  //     router.push("/");
-  //   }
-  // }, [auth, router]);
-
-  // if (!auth) {
-  //   return null;
-  // }
+  useEffect(() => {
+    if (!auth) {
+      router.push("/");
+    }
+  }, [auth, router]);
 
   // Logout from dashboard
   const handleLogOut = () => {
@@ -29,20 +25,22 @@ export default function AdminProfile() {
     router.push("/Auth/Admin/LogIn");
   };
 
-  // const { _id, first_name, last_name, email_id, user_type } = auth;
-
   return (
     <PrivateRoute>
       <div>
         <h3 className="text-right p-5 cursor-pointer" onClick={handleLogOut}>
           Logout
         </h3>
-        {/* <h2>Hello @{first_name}</h2>
-        <div>ID: {_id}</div>
-        <div>First Name: {first_name}</div>
-        <div>Last Name: {last_name}</div>
-        <div>Email: {email_id}</div>
-        <div>User Type: {user_type}</div> */}
+        {authData && (
+          <>
+            <h2>Hello @{authData.first_name}</h2>
+            <div>ID: {authData._id}</div>
+            <div>First Name: {authData.first_name}</div>
+            <div>Last Name: {authData.last_name}</div>
+            <div>Email: {authData.email_id}</div>
+            <div>User Type: {authData.user_type}</div>
+          </>
+        )}
       </div>
     </PrivateRoute>
   );
--- src/app/lib/slices/adminSlice.js
+++ src/app/lib/slices/adminSlice.js
@@ -1,100 +1,112 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import authService from "../services/authServices";
 
-const admin = typeof window !== 'undefined' ? JSON.parse(localStorage.getItem('admin')) : null
+const admin =
+  typeof window !== "undefined"
+    ? JSON.parse(localStorage.getItem("admin"))
+    : null;
 
 // Auth Registration
 export const AuthRegistration = createAsyncThunk(
-    "auth/register",
-    async ({ first_name, last_name, email_id, phone_number, password, toast, router }, thunkAPI) => {
-        try {
-            const response = await authService.authregister(first_name, last_name, email_id, phone_number, password);
-            toast.success("Registration Successfully");
-            // Delay the redirection by 1.5 seconds
-            await new Promise(resolve => setTimeout(resolve, 1500));
-            router.push("/Auth/Admin/Profile");
-            return response.data.user;
-        } catch (error) {
-            return thunkAPI.rejectWithValue();
-        }
+  "auth/register",
+  async (
+    { first_name, last_name, email_id, phone_number, password, toast, router },
+    thunkAPI
+  ) => {
+    try {
+      const response = await authService.authregister(
+        first_name,
+        last_name,
+        email_id,
+        phone_number,
+        password
+      );
+      toast.success("Registration Successfully");
+      // Delay the redirection by 1.5 seconds
+      await new Promise((resolve) => setTimeout(resolve, 1500));
+      router.push("/Auth/Admin/Profile");
+      return response.data.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue();
     }
+  }
 );
 
 // Auth Login
 export const AuthLogin = createAsyncThunk(
-    "auth/login",
-    async ({ email_id, password, toast, router }, thunkAPI) => {
-        try {
-            const response = await authService.authlogin(email_id, password);
-            toast.success("Login Successfully");
-            // Delay the redirection by 1.5 seconds
-            await new Promise(resolve => setTimeout(resolve, 1500));
-            router.push("/Auth/Admin/Profile");
-            return response.data;
-        } catch (error) {
-            return thunkAPI.rejectWithValue();
-        }
+  "auth/login",
+  async ({ email_id, password, toast, router }, thunkAPI) => {
+    try {
+      const response = await authService.authlogin(email_id, password);
+      toast.success("Login Successfully");
+      // Delay the redirection by 1.5 seconds
+      await new Promise((resolve) => setTimeout(resolve, 1500));
+      router.push("/Auth/Admin/Profile");
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue();
     }
+  }
 );
 
 export const authLogout = createAsyncThunk("auth/logout", async () => {
-    await authService.authlogout();
+  await authService.authlogout();
 });
 
 const initialState = {
-    isLoggedIn: !!admin,
-    admin: admin || null,
-    error: "",
-    loading: false,
+  isLoggedIn: !!admin,
+  admin: admin || null,
+  error: "",
+  loading: false,
 };
 
 const adminSlice = createSlice({
-    name: "admin",
-    initialState,
-    reducers: {
-        setUser: (state, action) => {
-            state.admin = action.payload;
-        },
-        setLogout: (state) => {
-            localStorage.removeItem("admin");
-            state.admin = null;
-        },
+  name: "admin",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      state.admin = action.payload;
     },
-    extraReducers: (builder) => {
-        builder
-            .addCase(AuthRegistration.pending, (state) => {
-                state.loading = true;
-            })
-            .addCase(AuthRegistration.fulfilled, (state, action) => {
-                state.loading = false;
-                state.admin = { data: { user: action.payload } };  // Ensure direct assignment
-                state.isLoggedIn = true;
-                localStorage.setItem("admin", JSON.stringify(state.admin));
-            })
-            .addCase(AuthRegistration.rejected, (state, action) => {
-                state.loading = false;
-                state.error = action.payload;
-            })
-            .addCase(AuthLogin.pending, (state) => {
-                state.loading = true;
-            })
-            .addCase(AuthLogin.fulfilled, (state, action) => {
-                state.loading = false;
-                state.admin = action.payload;
-                state.isLoggedIn = true;
-                localStorage.setItem("admin", JSON.stringify(action.payload));
-            })
-            .addCase(AuthLogin.rejected, (state, action) => {
-                state.loading = false;
-                state.error = action.payload;
-            })
-            .addCase(authLogout.fulfilled, (state) => {
-                state.loading = false;
-                localStorage.removeItem("admin");
-                state.admin = null;
-                state.isLoggedIn = false;
-            });
+    setLogout: (state) => {
+      localStorage.removeItem("admin");
+      state.admin = null;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(AuthRegistration.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(AuthRegistration.fulfilled, (state, action) => {
+        state.loading = false;
+        state.admin = { data: { user: action.payload } }; // Ensure direct assignment
+        state.isLoggedIn = true;
+        localStorage.setItem("admin", JSON.stringify(state.admin));
+      })
+      .addCase(AuthRegistration.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      .addCase(AuthLogin.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(AuthLogin.fulfilled, (state, action) => {
+        state.loading = false;
+        state.admin = action.payload;
+        state.isLoggedIn = true;
+        localStorage.setItem("admin", JSON.stringify(action.payload));
+      })
+      .addCase(AuthLogin.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      .addCase(authLogout.fulfilled, (state) => {
+        state.loading = false;
+        localStorage.removeItem("admin");
+        state.admin = null;
+        state.isLoggedIn = false;
+      });
+  },
 });
 
 export const { setUser, setLogout } = adminSlice.actions;

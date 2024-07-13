29bcef0cbccd002775f9012a411992f02084b584
ApVarotariya <apvarotariya17@gmail.com>
2024-07-13 17:24:45 +0530

    change in backend url

--- src/app/Readme.jsx
+++ src/app/Readme.jsx
@@ -1,3 +1,5 @@
-export const BASE_API_URL = "http://localhost/wordpressheadless/server/wp-json/wp/v2";
-export const LOCAL_BASE_API_URL = "http://localhost:8080/api";
+export const BASE_API_URL =
+  "http://localhost/wordpressheadless/server/wp-json/wp/v2";
+// export const LOCAL_BASE_API_URL = "http://localhost:8080/api";
+export const LOCAL_BASE_API_URL = "https://simply-hrm-main.onrender.com";
 // export const LOCAL_BASE_API_URL = "https://next-hrm-backend.vercel.app/api";

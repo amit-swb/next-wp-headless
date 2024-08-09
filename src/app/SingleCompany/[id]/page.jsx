042beba74c7c7bdcb37eae8073ff9323f7f4bc3b
amit-swb <amit@silverwebbuzz.com>
2024-08-09 18:48:39 +0530

    dfdf

--- src/app/SingleCompany/[id]/page.jsx
+++ src/app/SingleCompany/[id]/page.jsx
@@ -1,20 +1,4 @@
-"use client";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getSingleCompany } from "@/lib/slices/companySlice";
-import { useParams } from "next/navigation";
-
 const SingleCompany = () => {
-  // const dispatch = useDispatch();
-  // const { id } = useParams();
-  // const { singlecompany } = useSelector((state) => state.company);
-
-  // useEffect(() => {
-  //   if (id) {
-  //     dispatch(getSingleCompany({ id }));
-  //   }
-  // }, [id]);
-
   return <></>;
 };
 

f08e05df574033b2d7102c81c48741a70d2c5a1d
amit-swb <amit@silverwebbuzz.com>
2024-08-09 19:09:55 +0530

    sdfsdf

--- src/Components/PrivateRoute/PrivateRoute.js
+++ src/Components/PrivateRoute/PrivateRoute.js
@@ -1,21 +1,21 @@
-// import { useEffect } from "react";
-// import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
-// const PrivateRoute = ({ children }) => {
-//   const router = useRouter();
+const PrivateRoute = ({ children }) => {
+  const router = useRouter();
 
-//   useEffect(() => {
-//     const admin = localStorage.getItem("admin");
-//     const company = localStorage.getItem("companyToken");
-//     // const employee = localStorage.getItem("employeeToken");
-//     // const hr = localStorage.getItem("employeeToken");
+  useEffect(() => {
+    const admin = localStorage.getItem("admin");
+    const company = localStorage.getItem("companyToken");
+    // const employee = localStorage.getItem("employeeToken");
+    // const hr = localStorage.getItem("employeeToken");
 
-//     if (!admin && !company) {
-//       router.push("/");
-//     }
-//   }, [router]);
+    // if (!admin && !company) {
+    //   router.push("/");
+    // }
+  }, [router]);
 
-//   return children;
-// };
+  return children;
+};
 
-// export default PrivateRoute;
+export default PrivateRoute;

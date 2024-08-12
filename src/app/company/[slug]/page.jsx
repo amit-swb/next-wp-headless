92e78697822e6c5588f28bdf377c5fe92b8b1697
amit-swb <amit@silverwebbuzz.com>
2024-08-12 16:46:02 +0530

    sdfsdf

--- src/app/company/[slug]/page.jsx
+++ src/app/company/[slug]/page.jsx
@@ -2,12 +2,16 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getSingleCompany } from "@/lib/slices/companySlice";
-import { useParams } from "next/navigation";
+// import { useParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 const SingleCompany = () => {
   const [isClient, setIsClient] = useState(false);
   const dispatch = useDispatch();
-  const params = useParams(); // Use useParams to get the slug
+  const pathname = usePathname();
+  const match = pathname.match(/\/([^\/]+)$/);
+  const slug = match ? match[1] : "";
 
   const { singlecompany } = useSelector((state) => state.company);
 
@@ -16,16 +20,16 @@ const SingleCompany = () => {
   }, []);
 
   useEffect(() => {
-    if (params.slug && isClient) {
-      dispatch(getSingleCompany({ id: params.slug })); // Assuming id and slug are the same
+    if (pathname && isClient) {
+      dispatch(getSingleCompany({ id: pathname })); 
     }
-  }, [params.slug, isClient]);
+  }, [pathname, isClient]);
 
-  if (!isClient || !params.slug) return null; // Avoid rendering until client-side and slug are ready
+  if (!isClient || !pathname) return null; 
 
   return (
     <section className="bg-white dark:bg-gray-900">
-      <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">{/* ... the rest of your rendering logic ... */}dfsdf</div>
+      <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">dfsdf</div>
     </section>
   );
 };

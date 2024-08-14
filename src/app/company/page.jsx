f701ffa382922baa30634dedaba5cd581fd93daa
amit-swb <amit@silverwebbuzz.com>
2024-08-14 15:00:39 +0530

    sdfsfsfsfsf

--- src/app/company/page.jsx
+++ src/app/company/page.jsx
@@ -2,15 +2,21 @@
 import { useSearchParams } from "next/navigation";
 import { Suspense } from "react";
 
-const Company = () => {
+const SingleCompanyScreen = () => {
   const searchParams = useSearchParams();
   const id = searchParams.get("id");
 
   return (
+    <section className="bg-white dark:bg-gray-900">
+      <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">single company{id}</div>
+    </section>
+  );
+};
+
+const Company = () => {
+  return (
     <Suspense fallback={<p>Loading....</p>}>
-      <section className="bg-white dark:bg-gray-900">
-        <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">single company{id}</div>
-      </section>
+      <SingleCompanyScreen />
     </Suspense>
   );
 };

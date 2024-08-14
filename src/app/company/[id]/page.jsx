6fa229556dcd97d855948d9c7123770f132f9660
amit-swb <amit@silverwebbuzz.com>
2024-08-14 12:52:07 +0530

    dfdfdfdfdfdf

--- src/app/company/[id]/page.jsx
+++ src/app/company/[id]/page.jsx
@@ -1,12 +1,10 @@
 "use client";
 import React from "react";
 
-const Company = ({ params }) => {
-  const { id } = params;
-
+const Company = () => {
   return (
     <section className="bg-white dark:bg-gray-900">
-      <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">single company{id}</div>
+      <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">single company</div>
     </section>
   );
 };

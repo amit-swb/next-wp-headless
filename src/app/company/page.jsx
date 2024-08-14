dfafc0ffc4e1e07bc1de2346c0d8da48d7956bb9
amit-swb <amit@silverwebbuzz.com>
2024-08-14 11:18:11 +0530

    sdfsdf

--- src/app/company/page.jsx
+++ src/app/company/page.jsx
@@ -22,7 +22,7 @@ const SingleCompany = ({}) => {
 
   return (
     <section className="bg-white dark:bg-gray-900">
-      <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">{singlecompany ? <div>{singlecompany.name}</div> : "Loading..."}dfgdgdfg</div>
+      <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">{singlecompany ? <div>{singlecompany.name}</div> : "Loading..."}sjfhsjfhsjf</div>
     </section>
   );
 };
